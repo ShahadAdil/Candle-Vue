e45800dd31cd4e6ceab8db881625357ceec042a7
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-02-19 18:13:04 +0100

    candle

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,12 @@
 import NavBar from './components/NavBar.vue'
 // import Footer from "./components/Footer.vue";
 
-
-
-
 </script>
 
 <template>
-
 <NavBar />
 
 </template>
 
 <style scoped>
-
 </style>
